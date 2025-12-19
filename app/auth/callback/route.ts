df34dd9d223c0f3d8aa46d90cb5dd78b4172b442
aac2324 <161326407+aac2324@users.noreply.github.com>
2025-12-20 00:39:22 +0100

    changes

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -1,16 +1,16 @@
 import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs';
-
 import { cookies } from 'next/headers';
 import { NextResponse } from 'next/server';
 
 export async function GET(req: Request) {
-  const supabase = createRouteHandlerClient({ cookies });
-  const { searchParams } = new URL(req.url);
-  const code = searchParams.get("code");
+  const requestUrl = new URL(req.url);
+  const code = requestUrl.searchParams.get('code');
 
   if (code) {
+    const supabase = createRouteHandlerClient({ cookies });
     await supabase.auth.exchangeCodeForSession(code);
   }
 
-  return NextResponse.redirect("/");
+  // Redirect zur Origin-URL (z.B. https://nextjs-hiwise.vercel.app)
+  return NextResponse.redirect(requestUrl.origin);
 }
